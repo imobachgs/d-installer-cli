9cd5fb9d20e15ce7decc2ee8c301d060d2ec14f1
Imobach González Sosa <igonzalezsosa@suse.com>
2022-08-03 06:05:38 +0100

    Display country flags when listing languages

--- d-installer-cli/src/language.rs
+++ d-installer-cli/src/language.rs
@@ -1,5 +1,6 @@
 use crate::commands::{LanguageArgs, LanguageCommands};
-use d_installer_lib::{Client, LanguageClient};
+use d_installer_lib::{Client, LanguageClient, Language};
+use country_emoji::flag;
 
 pub fn handle(command: &LanguageArgs) {
     let client = Client::new().unwrap();
@@ -19,11 +20,7 @@ pub fn handle(command: &LanguageArgs) {
 
 fn list_languages(client: &LanguageClient) {
     match client.available_languages() {
-        Ok(languages) => {
-            for lang in languages {
-                println!("{} {}", &lang.id, &lang.name);
-            }
-        }
+        Ok(languages) => print_languages_list(&languages),
         Err(e) => println!("Error: {:?}", e),
     }
 }
@@ -36,11 +33,25 @@ fn select_language(client: &LanguageClient, langs: Vec<&str>) {
 
 fn get_languages(client: &LanguageClient) {
     match client.selected() {
-        Ok(languages) => {
-            for lang in languages {
-                println!("{} {}", &lang.id, &lang.name);
-            }
-        },
+        Ok(languages) => print_languages_list(&languages),
         Err(e) => println!("Error: {:?}", e)
+    }
+}
+
+fn print_languages_list(languages: &Vec<Language>) {
+    for lang in languages {
+        let mut parts = lang.id.split("_");
+
+        let lang_flag = match parts.nth(1) {
+            Some(code) => flag(code),
+            _ => None
+        };
+
+        println!(
+            "{} {} {}",
+            lang_flag.unwrap_or("-".to_string()),
+            lang.id,
+            lang.name
+        )
     }
 }
